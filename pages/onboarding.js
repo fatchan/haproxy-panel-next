b241e3d4fc9cde633a3af5430700894f3d4cca93
Thomas Lynch <tom@69420.me>
2023-03-03 22:49:50 +1100

    make react happy

--- pages/onboarding.js
+++ pages/onboarding.js
@@ -11,29 +11,29 @@ export default function Onboarding() {
 		<h5 className="fw-bold">Onboarding</h5>
 		<h6>(Under Construction... 🏗️)</h6>
 
-		<div class="list-group">
-			<div class="list-group-item d-flex gap-3">
-				<input class="form-check-input flex-shrink-0" type="checkbox" value="" />
-				<span class="pt-1 form-checked-content">
+		<div className="list-group">
+			<div className="list-group-item d-flex gap-3">
+				<input className="form-check-input flex-shrink-0" type="checkbox" value="" />
+				<span className="pt-1 form-checked-content">
 					<strong>
 						<i className="bi-card-list pe-none me-2" width="1em" height="1em" />
 						1. Add your website domain(s)
 					</strong>
-					<span class="d-block text-body-secondary mt-3">
-						<p>Add your domains in the "domains" section, including subdomains. These are the domains that you want to protect with BasedFlare.</p>
+					<span className="d-block text-body-secondary mt-3">
+						<p>Add your domains in the &quot;domains&quot; section, including subdomains. These are the domains that you want to protect with BasedFlare.</p>
 						<p>For most websites, this would be the root domain i.e. <code>example.com</code>, and the www subdomain i.e. <code>www.example.com</code>.</p>
 					</span>
 					<small>(form goes here)</small>
 				</span>
 			</div>
-			<div class="list-group-item d-flex gap-3">
-				<input class="form-check-input flex-shrink-0" type="checkbox" value="" />
-				<span class="pt-1 form-checked-content">
+			<div className="list-group-item d-flex gap-3">
+				<input className="form-check-input flex-shrink-0" type="checkbox" value="" />
+				<span className="pt-1 form-checked-content">
 					<strong>
 						<i className="bi-globe2 pe-none me-2" width="1em" height="1em" />
 						2. Update DNS records
 					</strong>
-					<span class="d-block text-body-secondary mt-3">
+					<span className="d-block text-body-secondary mt-3">
 						<p>For each of your domains and subdomains, set the following A and AAAA records with your DNS provider:</p>
 						<code>A</code>:
 						<ul>
@@ -49,36 +49,36 @@ export default function Onboarding() {
 					</span>
 				</span>
 			</div>
-			<div class="list-group-item d-flex gap-3">
-				<input class="form-check-input flex-shrink-0" type="checkbox" value="" />
-				<span class="pt-1 form-checked-content">
+			<div className="list-group-item d-flex gap-3">
+				<input className="form-check-input flex-shrink-0" type="checkbox" value="" />
+				<span className="pt-1 form-checked-content">
 					<strong>
 						<i className="bi-hdd-network-fill pe-none me-2" width="1em" height="1em" />
 						3. Enter backend details
 					</strong>
-					<span class="d-block text-body-secondary mt-3">
-						<p>For each domain and subdomain, set the backend server IP address and port in the "backends" section.</p>
+					<span className="d-block text-body-secondary mt-3">
+						<p>For each domain and subdomain, set the backend server IP address and port in the &quot;backends&quot; section.</p>
 						<p>The backend address and port should be in ip:port format, e.g. <code>12.34.56.78:443</code>.</p>
 					</span>
 					<small>(form goes here)</small>
 				</span>
 			</div>
-			<div class="list-group-item d-flex gap-3">
-				<input class="form-check-input flex-shrink-0" type="checkbox" value="" />
-				<span class="pt-1 form-checked-content">
+			<div className="list-group-item d-flex gap-3">
+				<input className="form-check-input flex-shrink-0" type="checkbox" value="" />
+				<span className="pt-1 form-checked-content">
 					<strong>
 						<i className="bi-file-earmark-lock-fill pe-none me-2" width="1em" height="1em" />
 						3. Generate a HTTPS certificate <small>(optional, recommended)</small>
 					</strong>
-					<span class="d-block text-body-secondary mt-3">
-						<p>BasedFlare can generate and setup a HTTPS certificates for you using <a href="https://letsencrypt.org/" target="_blank">Let’s Encrypt</a>.</p>
-						<p>Enter the domain subject and altnames in the "HTTPS Certificates" section and hit generate.</p>
+					<span className="d-block text-body-secondary mt-3">
+						<p>BasedFlare can generate and setup a HTTPS certificates for you using <a href="https://letsencrypt.org/" rel="noreferrer" target="_blank">Let’s Encrypt</a>.</p>
+						<p>Enter the domain subject and altnames in the &quot;HTTPS Certificates&quot; section and hit generate.</p>
 						<p>Additionally, if you operate a self-managed cluster and add new servers, you can upload any generated certificates to the new servers seamlessly.</p>
 					</span>
 					<small>(form goes here)</small>
 				</span>
 			</div>
-			<div class="list-group-item d-flex gap-3">
+			<div className="list-group-item d-flex gap-3">
 				All done!
 			</div>
 		</div>
