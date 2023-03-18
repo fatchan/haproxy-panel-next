1d6f15326f7a5a4e0a69d678aaa0fc918b04a8ba
Thomas Lynch <tom@69420.me>
2023-03-18 11:25:30 +1100

    Allow ca cert and pinned fp/s to be separate, and ignore node_modules in dockerignore, vastly speeds up builds (duh)

--- agent.js
+++ agent.js
@@ -6,10 +6,8 @@ const agentOptions = {
 	rejectUnauthorized: !process.env.ALLOW_SELF_SIGNED_SSL,
 };
 
-if (process.env.PINNED_FP && process.env.CUSTOM_CA_PATH) {
+if (process.env.PINNED_FP) {
 	// console.log('Pinned fingerprint:', process.env.PINNED_FP);
-	// console.log('Private CA file path:', process.env.CUSTOM_CA_PATH);
-	agentOptions.ca = require('fs').readFileSync(process.env.CUSTOM_CA_PATH);
 	agentOptions.checkServerIdentity = (host, cert) => {
 		//TODO: host verification? e.g. tls.checkServerIdentity(host, cert);
 		// console.log('Checking:', cert.fingerprint256);
@@ -17,6 +15,10 @@ if (process.env.PINNED_FP && process.env.CUSTOM_CA_PATH) {
 			return new Error('Certificate not pinned');
 		}
 	}
+}
+if (process.env.CUSTOM_CA_PATH) {
+	// console.log('Private CA file path:', process.env.CUSTOM_CA_PATH);
+	agentOptions.ca = require('fs').readFileSync(process.env.CUSTOM_CA_PATH);
 }
 
 module.exports = new https.Agent(agentOptions);
