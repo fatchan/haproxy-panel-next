22fd2470a678de084bbc09c8cde34638f07bfb73
Thomas Lynch <tom@69420.me>
2023-04-30 13:29:34 +1000

    fix origin CSR command and escaping of \n

--- pages/csr.js
+++ pages/csr.js
@@ -46,7 +46,7 @@ export default function Csr(props) {
 			<p>
 				To generate a certificate signing request for your domain and/or subdomain(s):
 				<code>
-					{`openssl req  -new -nodes -subj "/CN=`}<strong>yourdomain.com</strong>{`/OU=OrganisationUnit/O=Organisation/L=Locality/ST=St/C=Co" -sha256 -extensions v3_req -reqexts SAN -keyout origin.key -out origin.csr -config <(cat /etc/ssl/openssl.cnf \<\(printf "[SAN]\nsubjectAltName=DNS:`}<strong>www.yourdomain.com</strong>{`")) -days 3650`}
+					{`openssl reqkey rsa:4096 -new -nodes -subj "/CN=`}<strong>yourdomain.com</strong>{`/OU=OrganisationUnit/O=Organisation/L=Locality/ST=St/C=Co" -sha256 -extensions v3_req -reqexts SAN -keyout origin.key -out origin.csr -config <(cat /etc/ssl/openssl.cnf \<\(printf "[SAN]\\nsubjectAltName=DNS:`}<strong>www.yourdomain.com</strong>{`"))`}
 				</code>
 			</p>
 
