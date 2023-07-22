2724dfe5156f652c6f8b6366a8e54d01584c1298
Thomas Lynch <tom@69420.me>
2023-07-22 12:55:51 +0000

    Add loki pruner job to call to loki a delete query for any hh labels that dont match a domain in any existing users domains

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -65,6 +65,19 @@ module.exports = {
 			env_production: {
 				"NODE_ENV": "production",
 			}
+		},
+		{
+			name: "loki-pruner",
+			script: "./loki/main.js",
+			log_date_format: "YYYY-MM-DD HH:mm:ss",
+			instances : "1",
+			exec_mode : "fork",
+			env: {
+				"NODE_ENV": "development"
+			},
+			env_production: {
+				"NODE_ENV": "production",
+			}
 		}
 	]
 }
---
+++ loki/main.js
@@ -0,0 +1,62 @@
+'use strict';
+
+process
+	.on('uncaughtException', console.error)
+	.on('unhandledRejection', console.error);
+
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env' });
+const db = require('../db.js');
+const base64Auth = Buffer.from(`loki:${process.env.LOKI_AUTH}`).toString("base64");
+
+async function main() {
+	await db.connect();
+	loop();
+}
+
+const getLokiDomains = () => fetch(`${process.env.LOKI_HOST}loki/api/v1/label/hh/values`, {
+		withCredentials: true,
+		credentials: 'include',
+		headers: {
+			'Authorization': `Basic ${base64Auth}`
+		}
+	})
+	.then(res => res.json())
+	.then(res => res.data);
+
+const deleteLokiLabel = (value, label="hh") => fetch(`${process.env.LOKI_HOST}loki/api/v1/delete?query={${label}="${encodeURIComponent(value)}"}&start=1970-01-01T00:00:00.000Z`, {
+		method: 'POST',
+		withCredentials: true,
+		credentials: 'include',
+		headers: {
+			'Authorization': `Basic ${base64Auth}`
+		}
+	});
+
+async function loop() {
+	const start = Date.now();
+	try {
+		const lokiDomains = await getLokiDomains();
+		let userDomains = await db.db.collection('accounts')
+			.find({}, { projection: { domains: 1 } })
+			.toArray();
+		userDomains = userDomains.reduce((acc, account) => {
+				return acc.concat(account.domains||[]);;
+			}, []);
+		const userDomainsSet = new Set(userDomains)
+		lokiDomains.forEach(d => {
+			if (!userDomainsSet.has(d)) {
+				console.log('Deleting loki logs for untracked domain', d);
+				deleteLokiLabel(d);
+			}
+		})
+	} catch(e) {
+		console.error(e);
+		setTimeout(loop, 60000);
+		return;
+	}
+	const elapsed = Date.now() - start;
+	setTimeout(loop, 3600000);
+}
+
+main();
