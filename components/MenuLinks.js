e4329078788412b334e2ebaf267ebb4269a57f75
Thomas Lynch <tom@69420.me>
2023-09-24 09:36:04 +0000

    More responsive active state on MenuLinks in sidebar

--- components/MenuLinks.js
+++ components/MenuLinks.js
@@ -7,8 +7,12 @@ if ('default' in ResolvedImage) {
 }
 import Link from 'next/link';
 import { withRouter } from 'next/router';
+import { useState } from 'react';
+import Router from 'next/router';
 
 export default withRouter(function MenuLinks({ router }) {
+	const [path, setPath] = useState(router.pathname);
+	Router.events.on('routeChangeStart', setPath);
 	return (<>
 		<Link href='/' className='d-flex align-items-center mb-3 mb-md-0 text-body text-decoration-none'>
 			<ResolvedImage src='/favicon.ico' width='32' height='32' alt=' ' />
@@ -17,79 +21,79 @@ export default withRouter(function MenuLinks({ router }) {
 		<hr />
 		<ul className='nav nav-pills flex-column mb-auto'>
 			<li className='nav-item'>
-				<Link href='/account' className={router.pathname === '/account' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
+				<Link href='/account' className={path === '/account' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
 					<i className='bi-person-square pe-none me-2' width='16' height='16' />
 						Account
 				</Link>
 			</li>
 			<li className='nav-item'>
-				<Link href='/domains' className={router.pathname === '/domains' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
+				<Link href='/domains' className={path === '/domains' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
 					<i className='bi-layers pe-none me-2' width='16' height='16' />
 						Domains
 				</Link>
 			</li>
 			<li className='nav-item'>
-				<Link href='/map/hosts' className={router.pathname === '/map/[name]' && router.query.name === 'hosts' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
+				<Link href='/map/hosts' className={path === '/map/hosts' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
 					<i className='bi-hdd-network pe-none me-2' width='16' height='16' />
 						Backends
 				</Link>
 			</li>
 			<li className='nav-item'>
-				<Link href='/certs' className={router.pathname === '/certs' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
+				<Link href='/certs' className={path === '/certs' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
 					<i className='bi-file-earmark-lock pe-none me-2' width='16' height='16' />
 						HTTPS Certificates
 				</Link>
 			</li>
 			<li className='nav-item'>
-				<Link href='/csr' className={router.pathname === '/csr' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
+				<Link href='/csr' className={path === '/csr' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
 					<i className='bi-building-lock pe-none me-2' width='16' height='16' />
 						Origin CSR
 				</Link>
 			</li>
 			<li className='nav-item'>
-				<Link href='/map/ddos_config' className={router.pathname === '/map/[name]' && router.query.name === 'ddos_config' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
+				<Link href='/map/ddos_config' className={path === '/map/ddos_config' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
 					<i className='bi-sliders2 pe-none me-2' width='16' height='16' />
 						Protection Settings
 				</Link>
 			</li>
 			<li className='nav-item'>
-				<Link href='/map/ddos' className={router.pathname === '/map/[name]' && router.query.name === 'ddos' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
+				<Link href='/map/ddos' className={path === '/map/ddos' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
 					<i className='bi-shield-check pe-none me-2' width='16' height='16' />
 						Protection Rules
 				</Link>
 			</li>
 			<li className='nav-item'>
-				<Link href='/map/rewrite' className={router.pathname === '/map/[name]' && router.query.name === 'rewrite' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
+				<Link href='/map/rewrite' className={path === '/map/rewrite' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
 					<i className='bi-pencil pe-none me-2' width='16' height='16' />
 						Rewrites
 				</Link>
 			</li>
 			<li className='nav-item'>
-				<Link href='/map/redirect' className={router.pathname === '/map/[name]' && router.query.name === 'redirect' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
+				<Link href='/map/redirect' className={path === '/map/redirect' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
 					<i className='bi-signpost pe-none me-2' width='16' height='16' />
 						Redirects
 				</Link>
 			</li>
 			<li className='nav-item'>
-				<Link href='/map/whitelist' className={router.pathname === '/map/[name]' && router.query.name === 'whitelist' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
+				<Link href='/map/whitelist' className={path === '/map/whitelist' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
 					<i className='bi-person-check pe-none me-2' width='16' height='16' />
 						IP Whitelist
 				</Link>
 			</li>
 			<li className='nav-item'>
-				<Link href='/map/blockedip' className={router.pathname === '/map/[name]' && router.query.name === 'blockedip' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
+				<Link href='/map/blockedip' className={path === '/map/blockedip' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
 					<i className='bi-person-slash pe-none me-2' width='16' height='16' />
 						IP Blacklist
 				</Link>
 			</li>
 			<li className='nav-item'>
-				<Link href='/map/blockedasn' className={router.pathname === '/map/[name]' && router.query.name === 'blockedasn' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
+				<Link href='/map/blockedasn' className={path === '/map/blockedasn' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
 					<i className='bi-building-slash pe-none me-2' width='16' height='16' />
 						ASN Blacklist
 				</Link>
 			</li>
 			<li className='nav-item'>
-				<Link href='/map/maintenance' className={router.pathname === '/map/[name]' && router.query.name === 'maintenance' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
+				<Link href='/map/maintenance' className={path === '/map/maintenance' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
 					<i className='bi-info-square pe-none me-2' width='16' height='16' />
 						Maintenance Mode
 				</Link>
@@ -98,7 +102,7 @@ export default withRouter(function MenuLinks({ router }) {
 		<hr />
 		<ul className='nav nav-pills flex-column'>
 			<li className='nav-item user-select-none'>
-				<Link href='/onboarding' className={router.pathname === '/onboarding' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
+				<Link href='/onboarding' className={path === '/onboarding' ? 'nav-link active' : 'nav-link text-body'} aria-current='page'>
 					<i className='bi-rocket-takeoff pe-none me-2' width='16' height='16' />
 						Onboarding
 				</Link>
