8a88a5830378e14ee71641c4ba01ad381064406a
Thomas Lynch <tom@69420.me>
2024-07-28 04:30:35 +0000

    Update sync, add util to list maps and augment with the entry_cnt as "size"

--- sync/main.js
+++ sync/main.js
@@ -1,9 +1,11 @@
-'use strict';
+'use strict'; 
 
 process
 	.on('uncaughtException', console.error)
 	.on('unhandledRejection', console.error);
 
+import fetch from 'node-fetch';
+import https from 'https';
 import dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
 
@@ -23,6 +25,7 @@ const autodiscoverService = new AutodiscoverService();
 import agent from '../agent.js';
 
 const base64Auth = Buffer.from(`${process.env.DATAPLANE_USER}:${process.env.DATAPLANE_PASS}`).toString('base64');
+
 async function overwriteMap(url, mapName, entries) {
 	const controller = new AbortController();
 	const signal = controller.signal;
@@ -30,7 +33,7 @@ async function overwriteMap(url, mapName, entries) {
 		controller.abort();
 	}, 10000);
 	const queryString = new URLSearchParams({ map: mapName }).toString();
-	await fetch(`${url.protocol}//${url.host}/v3/services/haproxy/runtime/maps_entries?${queryString}`, {
+	return fetch(`${url.protocol}//${url.host}/v3/services/haproxy/runtime/maps_entries?${queryString}`, {
 		method: 'POST',
 		agent,
 		headers: {
@@ -40,16 +43,72 @@ async function overwriteMap(url, mapName, entries) {
 		signal,
 		body: JSON.stringify(entries),
 	})
-		.then(res => console.log(res.status))
+		.then(res => res.text())
+		.then(res => console.log(res))
 		.catch(err => console.error(err));
 };
 
+async function getMapEntries(url, mapName) {
+	const controller = new AbortController();
+	const signal = controller.signal;
+	setTimeout(() => {
+		controller.abort();
+	}, 10000);
+	const queryString = new URLSearchParams({ map: mapName }).toString();
+	return fetch(`${url.protocol}//${url.host}/v3/services/haproxy/runtime/maps_entries?${queryString}`, {
+		agent,
+		headers: {
+			'authorization': `Basic ${base64Auth}`,
+			'Content-Type': 'application/json',
+		},
+		signal,
+	})
+		.then(res => res.json())
+		.catch(err => console.error(err));
+};
+
+async function listMaps(url) {
+	const controller = new AbortController();
+	const signal = controller.signal;
+	setTimeout(() => {
+		controller.abort();
+	}, 10000);
+	return fetch(`${url.protocol}//${url.host}/v3/services/haproxy/runtime/maps`, {
+		agent,
+		headers: {
+			'authorization': `Basic ${base64Auth}`,
+			'Content-Type': 'application/json',
+		},
+		signal,
+	})
+		.then(res => res.json())
+		.then(jsonData => {
+			jsonData.forEach(obj => {
+			  const match = obj.description.match(/entry_cnt=(\d+)/);
+			  if (match) {
+			    const size = parseInt(match[1]);
+			    obj.size = size;
+			  }
+			});
+			return jsonData;
+		})
+		.catch(err => console.error(err));
+};
+
+
 async function main() {
 	try {
-		console.log('Running a sync for %d nodes', autodiscoverService.urls.length);
 		//TODO: push urls to task queue
-		//temp
-		await overwriteMap(autodiscoverService.urls[0], 'alt-svc.map', [{key:'x1',value:'y1'},{key:'x2',value:'y2'}]);
+
+		console.log((await listMaps(autodiscoverService.urls[0])));
+
+		// const entries = await getMapEntries(autodiscoverService.urls[0], 'alt-svc.map');
+		// console.log(entries)
+		// entries.push({ key: 'XX', value: 'h2="alt-af.bfcdn.host:443"; ma=600;' });
+		// console.log(entries)
+		// await overwriteMap(autodiscoverService.urls[0], 'alt-svc.map', [...entries]);
+		// console.log((await getMapEntries(autodiscoverService.urls[0], 'alt-svc.map')));
+
 	} catch(e) {
 		console.error(e);
 	}
