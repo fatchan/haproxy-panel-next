d163f1ea5404b6a5f90983e4956ce0528a5606d7
Thomas Lynch <tom@69420.me>
2025-03-28 10:28:50 +0000

    Log autodiscover count after haproxy tag filtering

--- autodiscover.js
+++ autodiscover.js
@@ -22,10 +22,10 @@ class AutodiscoverService {
 		try {
 			const response = await fetch(`${process.env.AUTODISCOVER_HOST}/v1/autodiscover`, fetchOptions);
 			const json = await response.json();
-			console.log('Autodiscovered %d hosts', json.length);
 			this.#autodiscoveredHosts = json
 				.filter(x => x.tags.includes('haproxy'))
 				.map(h => new URL(`https://${process.env.DATAPLANE_USER}:${process.env.DATAPLANE_PASS}@${h.hostname}:2001/`));
+			console.log('Autodiscovered %d hosts', this.#autodiscoveredHosts.length);				
 		} catch (error) {
 			console.error('Autodiscover failed:', error);
 		}
