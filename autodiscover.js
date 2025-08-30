781fbd4a5363b175444b7669287e46c90eeb1f16
Thomas Lynch <tom@69420.me>
2025-08-30 08:03:31 +0000

    Make autodiscover module fallback to statis of DEFAULT_CLUSTER for compatibility with more minimal installs

--- autodiscover.js
+++ autodiscover.js
@@ -4,8 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
 
 if (!process.env.AUTODISCOVER_HOST) {
-	console.warn('process.env.AUTODISCOVER_HOST not set, autodiscover server not running');
-	process.exit(1);
+	console.warn('process.env.AUTODISCOVER_HOST not set, using static DEFAULT_CLUSTER');
 }
 
 const base64Auth = Buffer.from(`${process.env.AUTODISCOVER_USER}:${process.env.AUTODISCOVER_PASS}`).toString('base64');
@@ -20,16 +19,22 @@ class AutodiscoverService {
 
 	async init () {
 		await this.autodiscover(); // Initial autodiscover
-		setInterval(() => this.autodiscover(), 60000); // Repeat autodiscover every 60 seconds
+		if (process.env.AUTODISCOVER_HOST) {
+			setInterval(() => this.autodiscover(), 60000); // Repeat autodiscover every 60 seconds
+		}
 	}
 
 	async autodiscover () {
 		try {
-			const response = await fetch(`${process.env.AUTODISCOVER_HOST}/v1/autodiscover`, fetchOptions);
-			const json = await response.json();
-			this.#autodiscoveredHosts = json
-				.filter(x => x.tags.includes('haproxy'))
-				.map(h => new URL(`https://${process.env.DATAPLANE_USER}:${process.env.DATAPLANE_PASS}@${h.hostname}:2001/`));
+			if (process.env.AUTODISCOVER_HOST) {
+				const response = await fetch(`${process.env.AUTODISCOVER_HOST}/v1/autodiscover`, fetchOptions);
+				const json = await response.json();
+				this.#autodiscoveredHosts = json
+					.filter(x => x.tags.includes('haproxy'))
+					.map(h => new URL(`https://${process.env.DATAPLANE_USER}:${process.env.DATAPLANE_PASS}@${h.hostname}:2001/`));
+			} else {
+				this.#autodiscoveredHosts = process.env.DEFAULT_CLUSTER.split(',').map(u => new URL(u));
+			}
 			console.log('Autodiscovered %d hosts', this.#autodiscoveredHosts.length);
 		} catch (error) {
 			console.error('Autodiscover failed:', error);
